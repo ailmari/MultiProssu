457fdd9672d12c933a888f49eca2e8681f6b4cad
TTO200120\artnieme <arttu.niemela@oulu.fi>
2018-03-19 12:47:50 +0200

    Basic pipeline works. Returns greyscale image (not resized).

--- MultiProssu/main_GPU.c
+++ MultiProssu/main_GPU.c
@@ -14,6 +14,8 @@ void printInfo(cl_platform_id platform_id, cl_device_id device_id);
 
 int main()
 {
+	cl_int status;
+
 	// Gets platform and device
 	cl_platform_id platform_id;
 	cl_device_id device_id;
@@ -27,11 +29,21 @@ int main()
 
 	// Creates context
 	printf("\nCreating context...\n");
-	cl_context context = clCreateContext(NULL, 1, &device_id, NULL, NULL, NULL);
+	cl_context context = clCreateContext(NULL, 1, &device_id, NULL, NULL, &status);
+	if (status != CL_SUCCESS)
+	{
+		printf("OpenCL error... %d\n", status);
+		return 1;
+	}
 
 	// Creates command queue
 	printf("Creating command queue...\n");
-	cl_command_queue command_queue = clCreateCommandQueue(context, device_id, 0, NULL);
+	cl_command_queue command_queue = clCreateCommandQueue(context, device_id, 0, &status);
+	if (status != CL_SUCCESS)
+	{
+		printf("OpenCL error... %d\n", status);
+		return 1;
+	}
 
 	// Decodes images
 	unsigned char* rgba_L;
@@ -49,53 +61,146 @@ int main()
 	// Creates OpenCL image objects
 	cl_image_format image_format;
 	image_format.image_channel_order = CL_RGBA;
-	image_format.image_channel_data_type = CL_UNORM_INT8;
+	image_format.image_channel_data_type = CL_UNSIGNED_INT8;
 
 	cl_image_desc image_desc;
 	image_desc.image_type = CL_MEM_OBJECT_IMAGE2D;
 	image_desc.image_width = w_in;
 	image_desc.image_height = h_in;
+	image_desc.image_depth = 8;
+	image_desc.image_row_pitch = w_in * 4;
+	image_desc.image_slice_pitch = 0;
+	image_desc.num_mip_levels = 0;
+	image_desc.num_samples = 0;
+	image_desc.buffer = NULL;
 
-	cl_mem img_L = clCreateImage(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, &image_format, &image_desc, rgba_L, NULL);
-	cl_mem img_R = clCreateImage(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, &image_format, &image_desc, rgba_R, NULL);
+	cl_mem img_L = clCreateImage(context, CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR, &image_format, &image_desc, rgba_L, &status);
+	if (status != CL_SUCCESS)
+	{
+		printf("OpenCL error... %d\n", status);
+		return 1;
+	}
+	cl_mem img_R = clCreateImage(context, CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR, &image_format, &image_desc, rgba_R, &status);
+	if (status != CL_SUCCESS)
+	{
+		printf("OpenCL error... %d\n", status);
+		return 1;
+	}
 
 	// Creates sampler
-	cl_sampler sampler = clCreateSampler(context, CL_FALSE, CL_ADDRESS_CLAMP_TO_EDGE, CL_FILTER_NEAREST, NULL);
+	cl_sampler sampler = clCreateSampler(context, CL_FALSE, CL_ADDRESS_CLAMP_TO_EDGE, CL_FILTER_NEAREST, &status);
+	if (status != CL_SUCCESS)
+	{
+		printf("OpenCL error... %d\n", status);
+		return 1;
+	}
 
 	// Load the kernel source code into the array source_str
 	FILE *fp;
 	char *source_str;
+	size_t source_size;
 
 	fp = fopen("resize_greyscale.cl", "r");
+	if (!fp) {
+		fprintf(stderr, "Failed to load kernel.\n");
+		exit(1);
+	}
 	source_str = (char*)malloc(MAX_SOURCE_SIZE);
+	source_size = fread(source_str, 1, MAX_SOURCE_SIZE, fp);
 	fclose(fp);
 
 	// Creates program
-	cl_program program = clCreateProgramWithSource(context, device_id, (const char**)&source_str, NULL, NULL);
+	cl_program program = clCreateProgramWithSource(context, 1, (const char**)&source_str, (const size_t *)&source_size, &status);
+	if (status != CL_SUCCESS)
+	{
+		printf("clCreateProgramWithSource error... %d\n", status);
+		return 1;
+	}
+
+	status = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
+	if (status != CL_SUCCESS)
+	{
+		printf("clBuildProgram error... %d\n", status);
+
+		if (status != CL_SUCCESS) {
+			char *buff_erro;
+			cl_int errcode;
+			size_t build_log_len;
+			errcode = clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG, 0, NULL, &build_log_len);
+			if (errcode) {
+				printf("clGetProgramBuildInfo failed at line %d\n", __LINE__);
+				exit(-1);
+			}
+
+			buff_erro = malloc(build_log_len);
+			if (!buff_erro) {
+				printf("malloc failed at line %d\n", __LINE__);
+				exit(-2);
+			}
+
+			errcode = clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG, build_log_len, buff_erro, NULL);
+			if (errcode) {
+				printf("clGetProgramBuildInfo failed at line %d\n", __LINE__);
+				exit(-3);
+			}
+
+			fprintf(stderr, "Build log: \n%s\n", buff_erro); //Be careful with  the fprint
+			free(buff_erro);
+			fprintf(stderr, "clBuildProgram failed\n");
+			exit(EXIT_FAILURE);
+		}
+		return 1;
+	}
 
 	// Buffers for images
-	cl_mem buff_L = clCreateBuffer(context, CL_MEM_READ_WRITE, w_in*h_in, 0, NULL);
-	cl_mem buff_R = clCreateBuffer(context, CL_MEM_READ_WRITE, w_in*h_in, 0, NULL);
+	cl_mem buff_L = clCreateBuffer(context, CL_MEM_READ_WRITE, w_in*h_in, 0, &status);
+	if (status != CL_SUCCESS)
+	{
+		printf("clCreateBuffer error... %d\n", status);
+		return 1;
+	}
+	cl_mem buff_R = clCreateBuffer(context, CL_MEM_READ_WRITE, w_in*h_in, 0, &status);
+	if (status != CL_SUCCESS)
+	{
+		printf("clCreateBuffer error... %d\n", status);
+		return 1;
+	}
 
 	// Creates kernel
-	cl_kernel kernel = clCreateKernel(program, "resize_greyscale", NULL);
+	cl_kernel kernel = clCreateKernel(program, "resize_greyscale", &status);
+	if (status != CL_SUCCESS)
+	{
+		printf("clCreateKernel error... %d\n", status);
+		return 1;
+	}
 	clSetKernelArg(kernel, 0, sizeof(cl_mem), &img_L);
 	clSetKernelArg(kernel, 1, sizeof(cl_mem), &img_R);
 	clSetKernelArg(kernel, 2, sizeof(cl_mem), &buff_L);
 	clSetKernelArg(kernel, 3, sizeof(cl_mem), &buff_R);
 	clSetKernelArg(kernel, 4, sizeof(cl_sampler), &sampler);
-	clSetKernelArg(kernel, 5, sizeof(cl_int), &w_out);
-	clSetKernelArg(kernel, 6, sizeof(cl_int), &h_out);
+	clSetKernelArg(kernel, 5, sizeof(cl_int), &w_in);
+	clSetKernelArg(kernel, 6, sizeof(cl_int), &h_in);
 
-	size_t localWorkSize[2] = { 16, 16 };
-	size_t globalWorkSize[2] = { w_out, h_out };
+	size_t localWorkSize[2] = { 35, 24 };
+	size_t globalWorkSize[2] = { w_in, h_in };
 
-	clEnqueueNDRangeKernel(command_queue, kernel, 2, NULL, globalWorkSize, localWorkSize, 0, NULL, NULL);
+	status = clEnqueueNDRangeKernel(command_queue, kernel, 2, NULL, globalWorkSize, localWorkSize, 0, NULL, NULL);
+	if (status != CL_SUCCESS)
+	{
+		printf("clEnqueueNDRangeKernel error... %d\n", status);
+		return 1;
+	}
 
-	unsigned char* disp = (unsigned char*)malloc(w_out * h_out);
-	clEnqueueReadImage(command_queue, buff_L, CL_TRUE, 0, w_out*h_out, 0, 0, disp, 0, NULL, NULL);
+	unsigned char* disp = (unsigned char*)malloc(w_in * h_in);
+	size_t region[3] = { w_in, h_in, 0 };
+	status = clEnqueueReadBuffer(command_queue, buff_L, CL_TRUE, 0, w_in*h_in, disp, 0, NULL, NULL);
+	if (status != CL_SUCCESS)
+	{
+		printf("clEnqueueReadImage error... %d\n", status);
+		return 1;
+	}
 
-	lodepng_encode_file("output/test.png", disp, w_out, h_out, LCT_GREY, 8);
+	lodepng_encode_file("output/test.png", disp, w_in, h_in, LCT_GREY, 8);
 
 	return 0;
 }
--- MultiProssu/resize_greyscale.cl
+++ MultiProssu/resize_greyscale.cl
@@ -1,20 +1,20 @@
 __kernel void resize_greyscale(__read_only image2d_t in_img_L,
 							__read_only image2d_t in_img_R,
-							__write_only image2d_t out_img_L,
-							__write_only image2d_t out_img_L,
+							__global uchar *out_img_L,
+							__global uchar *out_img_R,
 							sampler_t sampler,
 							int width,
 							int height)
 {
-	const int i = get_global_id(0);
-	const int j = get_global_id(1);
+	const int x = get_global_id(0);
+	const int y = get_global_id(1);
 
 	// Calculating corresponding indices in the original image
-    int2 orig_pos = {(4*j-1*(j > 0)), (4*i-1*(i > 0))};
+    int2 orig_pos = {x, y};
     // Grayscaling and resizing
     uint4 pxL = read_imageui(in_img_L, sampler, orig_pos);
     uint4 pxR = read_imageui(in_img_R, sampler, orig_pos);
     
-    out_img_L[i*width+j] = 0.2126*pxL.x+0.7152*pxL.y+0.0722*pxL.z;
-	out_img_R[i*width+j] = 0.2126*pxR.x+0.7152*pxR.y+0.0722*pxR.z;
+    out_img_L[y*width+x] = 0.2126*pxL.x+0.7152*pxL.y+0.0722*pxL.z;
+	out_img_R[y*width+x] = 0.2126*pxR.x+0.7152*pxR.y+0.0722*pxR.z;
 }
